f4d036690c5d2fcf2bbbfa1d1eebf8e77d0a7f9e
dewikhulbi <dewikhlb20@gmail.com>
2024-12-30 03:35:46 +0700

    Rute pertama yang dimulai adalah DestinasiHome.route. Di sini, HomeScreen akan ditampilkan,

---
+++ app/src/main/java/com/example/consumerestapi/ui/Navigasi/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.consumerestapi.ui.Navigasi
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.consumerestapi.ui.screens.DestinasiDetail
+import com.example.consumerestapi.ui.screens.DetailView
+import com.example.consumerestapi.ui.view.DestinasiEdit
+import com.example.consumerestapi.ui.view.DestinasiEntry
+import com.example.consumerestapi.ui.view.DestinasiHome
+import com.example.consumerestapi.ui.view.EditView
+import com.example.consumerestapi.ui.view.EntryMhsScreen
+import com.example.consumerestapi.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(
+            route = DestinasiHome.route
+        ){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { nim ->
+                    navController.navigate("${DestinasiDetail.route}/$nim")
+                    println(nim)
+                }
+            )
+        }
